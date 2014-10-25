63398920ddb72cdeac89e0ed9b7f67c43a1eefca
Nick Esposito <nickespo@Nicks-MacBook-Air.local>
2014-10-25 13:22:43 -0400

    changed expiring to a bool for 'is revoked'

--- models/token.rb
+++ models/token.rb
@@ -5,6 +5,6 @@ class Token
   property :name,       String,   :required => true, :default => lambda{|r,p|Supyoer.hash_val(Faker::Lorem.characters((6..12).to_a.sample))}
   property :phone_hash, String,   :required => true
   property :created_at, DateTime
-  property :expires_at, DateTime, :default  => lambda{|r,p|DateTime.now+1}
+  property :is_revoked, Boolean,  :default  => false
 
 end
